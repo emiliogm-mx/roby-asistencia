85d521a7d9ded41f2c91e92783cfddadfecb6e53
emiliogm-mx <emiliogm.mx@gmail.com>
2025-04-26 15:05:21 -0600

    Subida inicial del proyecto Roby Asistencia 1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Roby Asistencia 1.0
+
+Proyecto base para la App de Asistencia para Líderes de Tienda Roby.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+      <h1>Roby Asistencia 1.0</h1>
+      <p>¡Bienvenido al proyecto base!</p>
+    </div>
+  );
+}
+
+export default App;
