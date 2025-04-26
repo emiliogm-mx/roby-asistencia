412d3831e82a70c3434f310123d682028e78bc6f
emiliogm-mx <emiliogm.mx@gmail.com>
2025-04-26 15:52:30 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import SugerenciasCompra from './SugerenciasCompra';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sugerencias" element={<SugerenciasCompra />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
 
